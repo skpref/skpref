66febb1fa70a47d8d1739cd3af7f86c3d263d3a4
istvanp1 <istvan.p.g@gmail.com>
2019-12-05 08:02:15 +0000

    added test for sparse matrix

--- skpref/tests/test_data_processing.py
+++ skpref/tests/test_data_processing.py
@@ -17,5 +17,23 @@ class TestSubsetPosetVec(unittest.TestCase):
         np.testing.assert_array_equal(self.test_spv.entity_universe,
                                       np.array(['a', 'b', 'c', 'd']))
 
-    #def test_efficient_representation(self):
+    def test_efficient_representation(self):
+        correct_top_array = np.array([
+            [1, 1, 1, 0],
+            [1, 1, 0, 1],
+            [0, 1, 1, 1]
+        ])
+        
+        correct_boot_array = np.array([
+            [0, 0, 0, 1],
+            [0, 0, 1, 0],
+            [1, 0, 0, 0]
+        ])
 
+        np.testing.assert_array_equal(
+            self.test_spv.efficient_representation[0].A,
+            correct_top_array)
+
+        np.testing.assert_array_equal(
+            self.test_spv.efficient_representation[1].A,
+            correct_boot_array)
