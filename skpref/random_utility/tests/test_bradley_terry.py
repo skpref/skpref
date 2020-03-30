1857d0a4afc4d707f039d4a870c1716e8e178b52
istvanp1 <istvan.p.g@gmail.com>
2020-03-30 18:55:19 +0100

    changed deprecated pandas.utils.testing to pandas.testing

--- skpref/random_utility/tests/test_bradley_terry.py
+++ skpref/random_utility/tests/test_bradley_terry.py
@@ -2,7 +2,7 @@ import pandas as pd
 import unittest
 import numpy as np
 import pytest
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 from skpref.random_utility import (check_indexing_of_entities,
                                    get_distinct_entities,
                                    generate_entity_lookup,
