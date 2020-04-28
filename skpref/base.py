7124282966ade6d69da6316019599542c9f26869
istvanp1 <istvan.p.g@gmail.com>
2020-04-28 10:38:19 +0100

    added base files for model inheritance structure

---
+++ skpref/base.py
@@ -0,0 +1,176 @@
+from sklearn.base import BaseEstimator
+from skpref.task import PrefTask, PairwiseComparisonTask, ChoiceTask
+import pandas as pd
+from typing import List
+import numpy as np
+
+
+class Model(BaseEstimator):
+    """Base Class for all models"""
+    def fit_task_unpacker(self, task: PrefTask) -> dict:
+        pass
+
+    def predict_task_unpacker(self, task: PrefTask) -> dict:
+        pass
+
+    def fit(self, df_comb: pd.DataFrame, target: str,
+            df_i: pd.DataFrame = None, df_j: pd.DataFrame = None,
+            merge_columns: List[str] = None) -> None:
+        pass
+
+    def fit_task(self, task: PrefTask) -> None:
+        return self.fit(**self.fit_task_unpacker(task))
+
+    def predict(self, df: pd.DataFrame) -> List:
+        pass
+
+    def predict_task(self, task: PrefTask) -> List:
+        return self.predict(**self.predict_task_unpacker(task))
+
+
+class PairwiseComparisonModel(Model):
+    """
+    All pairwise models will take a pandas DataFrame with multi-index where
+    each index is an entity and object of comparison made. This table should
+    contain the target value and any other predictive features that the user
+    would like to use and store on the observational level, for example the
+    weather for a particular match.
+    """
+
+    def __init__(self, pairwise_red_args: dict = None):
+        if pairwise_red_args is None:
+            pairwise_red_args = {}
+        self.pairwise_red_args = pairwise_red_args
+
+    def task_indexing(self, task: PrefTask) -> \
+            (pd.DataFrame, pd.DataFrame, List[str]):
+        """
+        Re-indexes the pandas DataFrame that sits in the task
+        Parameters:
+        -----------
+        task: Choice type task
+
+        Returns:
+        ---------
+        The indexed DataFrame
+        """
+        input_merge_columns = None
+        secondary_re_indexed = None
+
+        if (task.secondary_table is not None) and (
+                task.annotations['features_to_use'] is not None):
+            if (len(np.intersect1d(task.secondary_table.columns,
+                                   task.annotations['features_to_use'])) > 0)\
+                    or (task.annotations['features_to_use'] == 'all'):
+
+                found_correspondence = False
+
+                for key in task.annotations['secondary_to_primary_link'].keys():
+                    value = task.annotations['secondary_to_primary_link'][key]
+                    if (value == [task.primary_table_target_name,
+                                  task.primary_table_alternatives_names] or
+                            value == [task.primary_table_alternatives_names,
+                                      task.primary_table_target_name] or
+                            value == task.primary_table_alternatives_names or
+                            value == task.primary_table_target_name):
+
+                        secondary_re_indexed = task.secondary_table.set_index(key)
+                        found_correspondence = True
+
+                    else:
+                        if input_merge_columns is None:
+                            input_merge_columns = [key]
+                        else:
+                            input_merge_columns += [key]
+
+                if not found_correspondence:
+                    raise Exception("key linking to alternatives not provided")
+
+        if type(task) is PairwiseComparisonTask:
+            _re_indexed_df = task.primary_table.set_index([
+                task.target_column_correspondence,
+                task.inverse_correspondence_column[0]
+            ])
+
+            _re_indexed_df.rename(columns={
+                task.annotations['primary_table_target_names']: 'alt1_top'},
+                inplace=True
+            )
+
+        elif type(task) is ChoiceTask:
+            pairwise_comparisons = task.subset_vec.pairwise_reducer(
+                **self.pairwise_red_args)
+            if task.annotations['features_to_use'] is not None:
+                feats_in_primary = []
+
+                if input_merge_columns is not None:
+                    feats_in_primary += input_merge_columns
+
+                if len(np.intersect1d(task.primary_table.columns,
+                                      task.annotations['features_to_use'])) > 0:
+
+                    feats_in_primary += np.intersect1d(
+                        task.primary_table.columns,
+                        task.annotations['features_to_use']).tolist()
+
+                if len(feats_in_primary) > 0:
+                    feats_in_primary = np.unique(feats_in_primary).tolist()
+                    pairwise_comparisons = pairwise_comparisons.merge(
+                        task.primary_table[feats_in_primary].reset_index()
+                        .rename(columns={'index': 'observation'}),
+                        how='left', on='observation', validate='m:1')
+
+            _re_indexed_df = pairwise_comparisons.set_index(['alt1', 'alt2'])
+
+        else:
+            raise Exception(f'Conversion from {type(task)} to Pairwise '
+                            f'Comparison models currently not supported')
+
+        if input_merge_columns is None:
+            input_merge_columns = []
+
+        return _re_indexed_df, secondary_re_indexed, input_merge_columns
+
+    def fit_task_unpacker(self, task: PrefTask) -> dict:
+
+        _re_indexed_df, secondary_re_indexed, input_merge_columns = \
+            self.task_indexing(task)
+
+        if task.annotations['features_to_use'] is None:
+
+            if type(task) is not PairwiseComparisonTask:
+                _re_indexed_df.drop(['observation'], axis=1, inplace=True)
+
+            model_input = _re_indexed_df[['alt1_top']].copy()
+            secondary_input = None
+
+        elif task.annotations['features_to_use'] != 'all':
+            model_input = _re_indexed_df[
+                ['alt1_top'] +
+                task.primary_table_features_to_use.tolist() +
+                input_merge_columns
+                ].copy()
+            secondary_input = secondary_re_indexed[
+                task.secondary_table_features_to_use.tolist() +
+                input_merge_columns
+                ].copy()
+        else:
+            model_input = _re_indexed_df.copy()
+            secondary_input = secondary_re_indexed.copy()
+
+        return {'df_comb': model_input,
+                'target': 'alt1_top',
+                'df_i': secondary_input,
+                'merge_columns': input_merge_columns}
+
+
+class GLMPairwiseComparisonModel(PairwiseComparisonModel):
+    def __init__(self):
+        super(GLMPairwiseComparisonModel, self).__init__()
+
+
+class SVMPairwiseComparisonModel(PairwiseComparisonModel):
+    def __init__(self):
+        super(SVMPairwiseComparisonModel, self).__init__(
+            {'style': 'reciprocal'}
+        )
---
+++ skpref/tests/test_base.py
@@ -0,0 +1,271 @@
+import unittest
+from skpref.task import ChoiceTask, PairwiseComparisonTask
+import pandas as pd
+from skpref.base import PairwiseComparisonModel, SVMPairwiseComparisonModel
+from pandas.testing import assert_frame_equal
+
+SUBSET_CHOICE_TABLE = pd.DataFrame(
+    {'choice': [[512709, 529703, 696056], [723354]],
+     'alternatives': [[512709, 529703, 696056, 490972,  685450, 5549502],
+                      [550707, 551375, 591842, 601195, 732624, 778197, 813892,
+                       817040, 576214, 673995, 723354]]}
+)
+
+SUBSET_CHOICE_FEATS_TABLE = pd.DataFrame(
+    {'ID': [490972,  512709,  529703,  550707,  551375,  576214,  591842,
+            601195,  673995,  685450,  696056,  723354,  732624,  778197,
+            813892,  817040, 5549502],
+     'feat1': [6, 6, 6, 8, 8, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+     }
+)
+
+SUBSET_CHOICE_TABLE_season = pd.DataFrame(
+    {'choice': [[512709, 529703, 696056], [723354]],
+     'alternatives': [[512709, 529703, 696056, 490972,  685450, 5549502],
+                      [550707, 551375, 591842, 601195, 732624, 778197, 813892,
+                       817040, 576214, 673995, 723354]],
+     'season': [7, 8]}
+)
+
+SUBSET_CHOICE_FEATS_TABLE_season = pd.DataFrame(
+    {'ID': [490972,  512709,  529703,  550707,  551375,  576214,  591842,
+            601195,  673995,  685450,  696056,  723354,  732624,  778197,
+            813892,  817040, 5549502,
+            490972, 512709, 529703, 550707, 551375, 576214, 591842,
+            601195, 673995, 685450, 696056, 723354, 732624, 778197,
+            813892, 817040, 5549502
+            ],
+     'feat1': [6, 6, 6, 8, 8, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     'season': [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8]
+     }
+)
+
+SUBSET_CHOICE_TABLE_feat = pd.DataFrame(
+    {'choice': [[512709, 529703, 696056], [723354]],
+     'alternatives': [[512709, 529703, 696056, 490972,  685450, 5549502],
+                      [550707, 551375, 591842, 601195, 732624, 778197, 813892,
+                       817040, 576214, 673995, 723354]],
+     'season': [7, 8]}
+)
+
+DATA = pd.DataFrame({'ent1': ['C', 'B', 'C', 'D'],
+                     'ent2': ['B', 'A', 'D', 'C'],
+                     'result': [1, 0, 0, 1]})
+
+ENT1_ATTRIBUTES = pd.DataFrame(
+    {'ent1': ['A', 'B', 'C', 'D'],
+     'feat1': [1, 11, 12, 15]}
+)
+
+
+class TestPairwiseComparisonModelFunctions(unittest.TestCase):
+
+    def test_task_indexing(self):
+        test_task = ChoiceTask(SUBSET_CHOICE_TABLE, 'alternatives', 'choice',
+                               secondary_table=SUBSET_CHOICE_FEATS_TABLE,
+                               secondary_to_primary_link={
+                                   'ID': ['choice', 'alternatives']},
+                               features_to_use='feat1')
+
+        mybt = PairwiseComparisonModel()
+
+        d1, d2, _ = mybt.task_indexing(test_task)
+
+        correct_d1 = test_task.subset_vec.pairwise_reducer() \
+            .set_index(['alt1', 'alt2'])
+
+        correct_d2 = SUBSET_CHOICE_FEATS_TABLE.set_index('ID')
+        assert_frame_equal(d1.astype('int32'), correct_d1.astype('int32'))
+        assert_frame_equal(d2.astype('int32'), correct_d2.astype('int32'))
+
+    def test_task_indexing_id_merge(self):
+        test_task = ChoiceTask(SUBSET_CHOICE_TABLE_season, 'alternatives',
+                               'choice',
+                               secondary_table=SUBSET_CHOICE_FEATS_TABLE_season,
+                               secondary_to_primary_link={
+                                   'ID': ['choice', 'alternatives'],
+                                   'season': 'season'},
+                               features_to_use=['feat1'])
+
+        mybt = PairwiseComparisonModel()
+
+        d1, d2, _id = mybt.task_indexing(test_task)
+
+        correct_d1 = test_task.subset_vec.pairwise_reducer() \
+            .set_index(['alt1', 'alt2'])
+
+        correct_d1['season'] = [7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8,
+                                8, 8, 8]
+
+        correct_d2 = SUBSET_CHOICE_FEATS_TABLE_season.set_index('ID')
+        correct_id = ['season']
+
+        assert_frame_equal(d1.astype('int32'), correct_d1.astype('int32'))
+        assert_frame_equal(d2.astype('int32'), correct_d2.astype('int32'))
+        self.assertListEqual(_id, correct_id)
+
+    def test_task_indexing_primary_feat_merge(self):
+        test_task = ChoiceTask(SUBSET_CHOICE_TABLE_feat, 'alternatives',
+                               'choice',
+                               secondary_table=SUBSET_CHOICE_FEATS_TABLE,
+                               secondary_to_primary_link={
+                                   'ID': ['choice', 'alternatives']},
+                               features_to_use=['feat1', 'season'])
+
+        mybt = PairwiseComparisonModel()
+
+        d1, d2, _id = mybt.task_indexing(test_task)
+
+        correct_d1 = test_task.subset_vec.pairwise_reducer() \
+            .set_index(['alt1', 'alt2'])
+
+        correct_d1['season'] = [7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8,
+                                8, 8, 8]
+
+        correct_d2 = SUBSET_CHOICE_FEATS_TABLE.set_index('ID')
+        correct_id = []
+
+        assert_frame_equal(d1.astype('int32'), correct_d1.astype('int32'))
+        assert_frame_equal(d2.astype('int32'), correct_d2.astype('int32'))
+        self.assertListEqual(_id, correct_id)
+
+    def test_task_indexing_id_also_feat_merge(self):
+        test_task = ChoiceTask(SUBSET_CHOICE_TABLE_feat, 'alternatives',
+                               'choice',
+                               secondary_table=SUBSET_CHOICE_FEATS_TABLE_season,
+                               secondary_to_primary_link={
+                                   'ID': ['choice', 'alternatives'],
+                                   'season': 'season'},
+                               features_to_use=['feat1', 'season'])
+
+        mybt = PairwiseComparisonModel()
+
+        d1, d2, _id = mybt.task_indexing(test_task)
+
+        correct_d1 = test_task.subset_vec.pairwise_reducer() \
+            .set_index(['alt1', 'alt2'])
+
+        correct_d1['season'] = [7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8,
+                                8, 8, 8]
+
+        correct_d2 = SUBSET_CHOICE_FEATS_TABLE_season.set_index('ID')
+        correct_id = ['season']
+
+        assert_frame_equal(d1.astype('int32'), correct_d1.astype('int32'))
+        assert_frame_equal(d2.astype('int32'), correct_d2.astype('int32'))
+        self.assertListEqual(_id, correct_id)
+
+    def test_task_indexing_pairwise_input(self):
+        test_task = PairwiseComparisonTask(
+            DATA, ['ent1', 'ent2'], 'result', 'ent1', features_to_use=None)
+
+        mybt = PairwiseComparisonModel()
+
+        d1, _, _ = mybt.task_indexing(test_task)
+
+        correct_d1 = DATA.set_index(['ent1', 'ent2'])\
+            .rename(columns={'result': 'alt1_top'})
+
+        assert_frame_equal(d1.astype('int32'), correct_d1.astype('int32'))
+
+    def test_task_indexing_pairwise_input_ents(self):
+        test_task = PairwiseComparisonTask(
+                DATA, ['ent1', 'ent2'], 'result', 'ent1',
+                secondary_table=ENT1_ATTRIBUTES,
+                secondary_to_primary_link={'ent1': ['ent1', 'ent2']})
+
+        mybt = PairwiseComparisonModel()
+
+        d1, d2, _ = mybt.task_indexing(test_task)
+
+        correct_d1 = DATA.set_index(['ent1', 'ent2']) \
+            .rename(columns={'result': 'alt1_top'})
+
+        correct_d2 = ENT1_ATTRIBUTES.set_index('ent1')
+
+        assert_frame_equal(d1.astype('int32'), correct_d1.astype('int32'))
+        assert_frame_equal(d2.astype('int32'), correct_d2.astype('int32'))
+
+    def test_fit_task_unpacker(self):
+        test_task = PairwiseComparisonTask(
+            DATA, ['ent1', 'ent2'], 'result', 'ent1',
+            secondary_table=ENT1_ATTRIBUTES,
+            secondary_to_primary_link={'ent1': ['ent1', 'ent2']})
+
+        mybt = PairwiseComparisonModel()
+
+        ret_dict = mybt.fit_task_unpacker(test_task)
+        correct_d1 = DATA.set_index(['ent1', 'ent2']) \
+            .rename(columns={'result': 'alt1_top'})
+        correct_d2 = ENT1_ATTRIBUTES.set_index('ent1')
+        correct_ret_dict = {
+            'df_comb': correct_d1,
+            'target': 'alt1_top',
+            'df_i': correct_d2,
+            'merge_columns': None
+        }
+
+        self.assertListEqual(list(ret_dict.keys()),
+                             list(correct_ret_dict.keys()))
+        assert_frame_equal(ret_dict['df_comb'], correct_d1)
+        self.assertEqual(correct_ret_dict['target'], 'alt1_top')
+        assert_frame_equal(ret_dict['df_i'], correct_d2)
+        self.assertEqual(correct_ret_dict['merge_columns'], None)
+
+
+
+class TestSVMPairwiseComparisonModel(unittest.TestCase):
+
+    def test_correct_indexing(self):
+        test_task = ChoiceTask(SUBSET_CHOICE_TABLE, 'alternatives', 'choice')
+
+        mybt = SVMPairwiseComparisonModel()
+
+        d1, _, _ = mybt.task_indexing(test_task)
+
+        correct_d1 = pd.DataFrame(
+            [
+                [0, 512709, 490972, 1],
+                [0, 490972, 512709, 0],
+                [0, 512709, 685450, 1],
+                [0, 685450, 512709, 0],
+                [0, 512709, 5549502, 1],
+                [0, 5549502, 512709, 0],
+                [0, 529703, 490972, 1],
+                [0, 490972, 529703, 0],
+                [0, 529703, 685450, 1],
+                [0, 685450, 529703, 0],
+                [0, 529703, 5549502, 1],
+                [0, 5549502, 529703, 0],
+                [0, 696056, 490972, 1],
+                [0, 490972, 696056, 0],
+                [0, 696056, 685450, 1],
+                [0, 685450, 696056, 0],
+                [0, 696056, 5549502, 1],
+                [0, 5549502, 696056, 0],
+                [1, 723354, 550707, 1],
+                [1, 550707, 723354, 0],
+                [1, 723354, 551375, 1],
+                [1, 551375, 723354, 0],
+                [1, 723354, 591842, 1],
+                [1, 591842, 723354, 0],
+                [1, 723354, 601195, 1],
+                [1, 601195, 723354, 0],
+                [1, 723354, 732624, 1],
+                [1, 732624, 723354, 0],
+                [1, 723354, 778197, 1],
+                [1, 778197, 723354, 0],
+                [1, 723354, 813892, 1],
+                [1, 813892, 723354, 0],
+                [1, 723354, 817040, 1],
+                [1, 817040, 723354, 0],
+                [1, 723354, 576214, 1],
+                [1, 576214, 723354, 0],
+                [1, 723354, 673995, 1],
+                [1, 673995, 723354, 0]
+            ], columns=['observation', 'alt1', 'alt2', 'alt1_top']
+        ).set_index(['alt1', 'alt2'])
+
+        assert_frame_equal(d1.astype('int32'), correct_d1.astype('int32'))
